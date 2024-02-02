734d944d44aca25f9804bedc4acdb25fa3fe14a8
masdo <158280858+masdogokui@users.noreply.github.com>
2024-02-02 13:24:06 +0100

    Create bateria.py

---
+++ bateria.py
@@ -0,0 +1,7 @@
+a = input("Introduzca su numero: ")
+b = input("Introduzca el segundo numero: ")
+suma = int(a) + int(b)
+print("el resultado de estos dos numeros es: ", suma)
+
+
+-[bateria1](bateria1.py)
